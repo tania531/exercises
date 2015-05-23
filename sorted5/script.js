621455cf9bdc556d37db3a007cd3e83b1de23589
Tania <tania.dev77@gmail.com>
2015-05-23 11:34:00 -0700

    ex 5 sorted fix

--- sorted5/script.js
+++ sorted5/script.js
@@ -1,8 +1,5 @@
-
 var result = find3LargestSorted("2, 4, 1, 9, 8, 10, 3, 16, 22, 8");
 console.log(result); // [22, 16, 10]
-
-// var numbers = [2, 4, 1, 9, 8, 10, 3, 16, 22, 8];
 
 function find3LargestSorted(str){
 var  arr = str.split(",");
