c4cedb5c111c7ac7573b6ec091f57c6a0a04aba3
Tania <tania.dev77@gmail.com>
2015-05-23 19:47:49 -0700

    fix random 14

--- random14/script.js
+++ random14/script.js
@@ -5,7 +5,7 @@ console.log('result: ', result); // [4, 5, 4, 4, 7, 8, 9, 3, 3, 8]
 function create10RandomNumbersBetween(a,z){
   var array10rand =[];
   for (var i = 0 ; i < 10 ; i++){
-    array10rand.push(Math.floor(Math.random()*(9-3+1)+3));
+    array10rand.push(Math.floor(Math.random()*(z-a+1)+a));
   }
   return array10rand;
 }
